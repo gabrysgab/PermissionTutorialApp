a00a7816e8bb31892412aac6822a2c0434a4c323
mateuszgabrynowicz <mateuszgabrynowicz@gmail.com>
2016-11-30 19:02:35 +0100

    layout improvements

--- app/src/main/java/com/mgabrynowicz/permissiontutorialapp/RegisterForm.java
+++ app/src/main/java/com/mgabrynowicz/permissiontutorialapp/RegisterForm.java
@@ -1,7 +1,9 @@
 package com.mgabrynowicz.permissiontutorialapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class RegisterForm extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class RegisterForm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_form);
     }
+
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        if (checked) {
+        }
+        else {
+        }
+
+    }
 }
+
+
+

dd4a0348ee7291f78e5095da4ef038f755fddc65
mateuszgabrynowicz <mateuszgabrynowicz@gmail.com>
2016-12-07 20:48:06 +0100

    added new welcome activity

---
+++ app/src/main/java/com/mgabrynowicz/permissiontutorialapp/Welcome_activity.java
@@ -0,0 +1,13 @@
+package com.mgabrynowicz.permissiontutorialapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_activity);
+    }
+}

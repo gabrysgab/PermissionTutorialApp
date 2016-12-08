72e0e9a299991ed8a2f3773ee52e80d343f19046
mateuszgabrynowicz <mateuszgabrynowicz@gmail.com>
2016-12-08 19:13:24 +0100

    added logo and text to welcome activity

--- app/src/main/java/com/mgabrynowicz/permissiontutorialapp/Welcome_activity.java
+++ app/src/main/java/com/mgabrynowicz/permissiontutorialapp/Welcome_activity.java
@@ -1,13 +1,39 @@
 package com.mgabrynowicz.permissiontutorialapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Welcome_activity extends AppCompatActivity {
+
+
+    FloatingActionButton registerFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_activity);
+
+        registerFloatingActionButton = (FloatingActionButton) findViewById(R.id.registerFloatingActionButton);
+
+        registerFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent startRegisterFormActivityIntent = new Intent(Welcome_activity.this, RegisterForm.class);
+                startActivity(startRegisterFormActivityIntent);
+
+
+            }
+        });
+
+
+
+
+
+
     }
 }

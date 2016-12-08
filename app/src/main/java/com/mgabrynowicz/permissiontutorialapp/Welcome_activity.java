7970d3becece9c5e68d1dd66e7f56af141e98ea6
mateuszgabrynowicz <mateuszgabrynowicz@gmail.com>
2016-12-08 20:43:37 +0100

    added drawer layout, navigation view

--- app/src/main/java/com/mgabrynowicz/permissiontutorialapp/Welcome_activity.java
+++ app/src/main/java/com/mgabrynowicz/permissiontutorialapp/Welcome_activity.java
@@ -1,22 +1,57 @@
 package com.mgabrynowicz.permissiontutorialapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
-public class Welcome_activity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class Welcome_activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     FloatingActionButton registerFloatingActionButton;
+
+    @BindView(R.id.main_toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.navigation)
+    NavigationView navigationView;
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_activity);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
         registerFloatingActionButton = (FloatingActionButton) findViewById(R.id.registerFloatingActionButton);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
 
         registerFloatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,9 +66,57 @@ public class Welcome_activity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
 
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
+        drawerLayout.closeDrawers();
 
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_settings_0:
+
+                return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_settings_0:
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
